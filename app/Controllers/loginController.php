cfd4171ba80409676dc4b429d52865b2386ea1e8
hatimSa <hatimsalhi03@gmail.com>
2024-12-29 15:35:29 +0100

    commit commit

--- app/Controllers/loginController.php
+++ app/Controllers/loginController.php
@@ -12,5 +12,27 @@ class LoginController extends BaseController
     public function attemptLogin()
     {
         // Logique de traitement du login
+        $session = session();
+
+        $model = model('UserModel');
+        $email = $this->request->getVar('Email');
+        $password = $this->request->getVar('password');
+
+        $user = $model->where('email', $email)->first();
+
+        if (!$user) {
+            $session->setFlashdata('msg', 'Email ou mot de passe incorrect');
+            return redirect()->to('/login');
+        }
+
+        $passHash = $user['password'];
+
+        if (password_verify($password, $passHash)) {
+            $session->set('loggedUser', $user);
+            return redirect()->to('/home');
+        }
+
+        $session->setFlashdata('msg', 'Email ou mot de passe incorrect');
+        return redirect()->to('/login');
     }
 }
