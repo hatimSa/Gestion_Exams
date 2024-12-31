46fa2eef7a6adec8dd8e612b4b18ad7c61469126
MeriemBELGRIRI <bmerry636@gmail.com>
2024-12-31 02:33:03 -0800

    test

--- app/Views/login.php
+++ app/Views/login.php
@@ -4,68 +4,209 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url('bootstrap-4.0.0-dist/css/bootstrap.min.css'); ?>">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+
+
+
     <title>Login Page</title>
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
+       
+        
+        
+         *{
+            margin:0;
+            padding:0;
+            box-sizing:border-box;
+            font-family:'Poppins','sans-serf';
         }
-
-        form {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        body{
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            min-height:100vh;
+            background:linear-gradient(90deg,#e2e2e2,#c9d6ff);
         }
+        .container{
+            position:relative;
+            width:850px;
+            height:550px;
+            background:#fff;
+            border-radius:30px;
+            box-shadow:0 0 30px rgba(0,0,0,.2);
+            overflow:hidden;
 
-        div {
-            margin-bottom: 15px;
         }
+        .form-box{
+            position: absolute;
+            right:0;
+            width:50%;
+            height:100%;
+            background:#fff;
+            display:flex;
+            align-items:center;
+            color:#333;
+            text-align:center;
+            padding: 40px;
+            z-index:1;
 
-        label {
-            display: block;
-            margin-bottom: 5px;
-            font-weight: bold;
         }
-
-        input[type="text"],
-        input[type="password"] {
-            width: 100%;
-            padding: 8px;
-            box-sizing: border-box;
-            border: 1px solid #ddd;
-            border-radius: 4px;
+        form{
+    
+            width:100%;
         }
+        .container h1{
+            font-size:36px;
+            margin:-10px 0 ;
 
-        button {
-            width: 100%;
-            padding: 10px;
-            background-color: #28a745;
-            color: #fff;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-            font-size: 16px;
         }
-
-        button:hover {
-            background-color: #218838;
+        .input-box{
+            position: relative;
+            margin:30px 0;
         }
+        .input-box input{
+            width:100%;
+            padding:13px 50px 13px 20px;
+            background:#eee;
+            border-radius:8px;
+            border:none;
+            outline:none;
+            font-size:16px;
+            color:#333;
+            font-weight:500;
+        }
+        .input-box input::placeholder{
+            color:#888;
+            font-weight:400;
 
-        .error {
-            color: red;
-            margin-bottom: 15px;
+        }
+        .input-box i{
+            position:absolute;
+            right:20px;
+            top:50%;
+            transform:translateY(-50%);
+            font-size:20px;
+            color:#888;
+
+        }
+        .forgot-link{
+            margin:-15px 0 15px;
+        }
+        .forgot-link a{
+            font-size:14.5px;
+            color:#333;
+            text-decoration:none;
+        }
+        .btn{
+            width:100%;
+            height:48px;
+            background:#7494ec;
+            border-radius:8px;
+            box-shadow:0 0 10px rgba(0,0,0, .1);
+            border:none;
+            cursor:pointer;
+            font-size:16px;
+            color:#fff;
+            font-weight:600;
+        }
+        .container p{
+            font-size:14,5px;
+            margin: 15px 0;
+
+        }
+        .social-icons a{
+            display:inline-flex;
+            padding:10px;
+            border:2px solid #ccc;
+            border-radius:8px;
+            font-size:24px;
+            color:#333;
+            text-decoration:none;
+            margin:0 8px;
+
+        }
+        .toggle-box{
+            position:absolute;
+            width:100%;
+            height:100%;
+        }
+        .toggle-box::before{
+            content:'';
+            position:absolute;
+            left: -250%;
+            width:300%;
+            height:100%;
+            background:#7494ec;
+            border-radius:150px;
+            z-index:2;
+
+        }
+        .toggle-panel{
+            position:absolute;
+            width:50%;
+            height:100%;
+            /* background:seagreen; */
+            color:#fff;
+            display:flex;
+            flex-direction:column;
+            justify-content:center;
+            align-items:center;
+            z-index:2;
+        }
+        .toggle-panel p{
+            margin-bottom:20px;
+        }
+        .toggle-panel .btn{
+            width:160px;
+            height:46px;
+            background:transparent;
+            border:2px solid #fff;
+            box-shadow:none;
         }
     </style>
 </head>
 
 <body>
-    <form action="<?= base_url('login') ?>" method="post">
+    <div class="container">
+        <div class="form-box login">
+            <form action="<?= base_url('login') ?>" method="post">
+        
+                <h1>Login</h1>
+                <div class="input-box">
+                    <input type="email" placeholder="email" name="email" required>
+                    <i class='bx bxs-user'></i>
+                </div>
+                <div class="input-box">
+                    <input type="password" placeholder="Passsword" name="password" required>
+                    <i class='bx bxs-lock'></i>
+                </div>
+                <div class="forgot-link">
+                    <a href="#">Forgot password</a>
+                    <button type="submit" class="btn">Login</button>
+                    <p>or login with social platforms</p>
+                    <div class="social-icons">
+                        <a href="#"><i class='bx bxl-google'></i></a>
+                        <a href="#"><i class='bx bxl-facebook'></i></a>
+                        <a href="#"><i class='bx bxl-github' ></i></a>
+                        <a href="#"><i class='bx bxl-linkedin' ></i></a>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+
+        <div class="toggle-box">
+            <div class="toggle-panel toggle-left">
+                <h1>Hello, Welcome!</h1>
+                <p>Don't have a account</p>
+                <button class="btn register-btn">Register</button>
+            </div>
+        </div>
+
+
+
+    </div>  
+    <!-- <form action="<?= base_url('login') ?>" method="post">
         <div>
             <?php if (session()->getFlashdata('error')): ?>
                 <div class="error"><?= session()->getFlashdata('error') ?></div>
@@ -78,7 +219,7 @@
             <input type="password" id="password" name="password" required placeholder="Password">
         </div>
         <button type="submit">Login</button>
-    </form>
+    </form> -->
 </body>
 
 </html>
