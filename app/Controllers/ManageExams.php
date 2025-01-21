fb8a1fee5ee6a26249c0b30f8e24fb1da9429c1c
MeriemBELGRIRI <bmerry636@gmail.com>
2025-01-21 00:06:33 -0800

    nv

--- app/Controllers/ManageExams.php
+++ app/Controllers/ManageExams.php
@@ -6,8 +6,8 @@ class ManageExams extends BaseController
 {
     public function index()
     {
-        // Aucune donnée à récupérer depuis la base de données
-        // Vous pouvez passer des données statiques si nécessaire, sinon laissez vide
+       
+        
         $data['currentPage'] = 'manageExams'; // Pour l'activation de la classe active sur la sidebar
 
         // Charger la vue avec les données
